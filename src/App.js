197a1b3d22101fa015ee941d802dfe2a97154bf2
Stephen Synowsky <ssynowsky@gmail.com>
2023-11-16 13:52:19 -0500

    Reverting mobile change; misunderstood request

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   useEffect(() => {
     let ctx = gsap.context(() => {
-      gsap.to('.desktop-container .arrow-down', {
+      gsap.to('.arrow-down', {
         keyframes: {
           '33%': {
             y: -10,
