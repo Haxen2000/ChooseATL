8ceba72ec023a53e656513f05bde65aa2af4f7e4
Stephen Synowsky <ssynowsky@gmail.com>
2023-11-16 14:25:32 -0500

    Making last section static on mobile

--- src/sections/mobile/section3-text-change-color/section3.js
+++ src/sections/mobile/section3-text-change-color/section3.js
@@ -21,14 +21,9 @@ function Section3 () {
           pinSpacerHeight={'3000px'}
           top={'25vh'}
         >
-          <div className='circle-text-change-anim flex flex-wrap'>
+          <div className='circle-text-change-anim flex flex-wrap relative'>
             <div className='img-container relative'>
               <Scrollytelling.Animation tween={{ start: 0, end: 70, to: {
-                width: '80vw',
-                height: '80vw',
-                borderRadius: 25,
-                backgroundSize: '272% 135.2%',
-                backgroundPosition: '-350px -100px',
                 onComplete: () => {
                   document.querySelector('.mobile-container .scroll-story').classList.add('hidden');
                 }
@@ -36,52 +31,14 @@ function Section3 () {
                 <div className='img-holder relative' />
               </Scrollytelling.Animation>
             </div>
-            <Scrollytelling.Animation tween={{ start: 0, end: 70, to: { top: '-80vw' } }}>
-              <svg className='svg-mask relative' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 600" width="100vw" height="80vw">
-                <defs>
-                  <clipPath id="theClipPathMobile">
-                    <Scrollytelling.Animation tween={{ start: 0, end: 70, to: { 
-                      width: 960,
-                      height: 960,
-                      rx: 240,
-                      y: 0,
-                      x: -180
-                    } }}>
-                  <rect className="masker" x="300" y="-180" rx='300' width="600" height="600" fill="rgba(255,0,0,.8)" />
-                    </Scrollytelling.Animation>
-                  </clipPath>
-                </defs> 
-                <foreignObject x="150" y="550" width="900" height="180">
-                  <div>
-                    <span className='quote'>You see that Atlanta isn't one thing.<br/>It's a multitude.<br/>
-                    </span>
-                  </div>
-                </foreignObject>
-                {/* <Scrollytelling.Animation tween={{ start: 0, end: 90, to: { 
-                  width: 960,
-                  height: 960,
-                  rx: 240,
-                  y: 0,
-                  x: -180
-                } }}>
-                  <rect className="masker" x="300" y="-180" rx='300' width="600" height="600" fill="rgba(255,0,0,.5)" />
-                </Scrollytelling.Animation> */}
-                <g clipPath="url(#theClipPathMobile)">
-                  <foreignObject x="150" y="550" width="900" height="180">
-                    <div>
-                      <span className='quote other'>You see that Atlanta isn't one thing.<br/>It's a multitude.<br/>
-                        <Scrollytelling.Animation tween={{ start: 30, end: 70, from: { width: 0, padding: 0 } }}>
-                          <div className='anim-text-holder'>
-                            <span className='anim-text peach'>Find your place in 2024.</span>
-                          </div>
-                        </Scrollytelling.Animation>
-                      </span>
-                    </div>
-                  </foreignObject>
-                </g>
-              </svg>
-            </Scrollytelling.Animation>
+            <div className='text-container absolute'>
+              <span className='quote other'>You see that Atlanta isn't one thing.<br/>It's a multitude.<br/>
+                <div className='anim-text-holder'>
+                  <span className='anim-text peach'>Find your place in 2024.</span>
+                </div>
+              </span>
             </div>
+          </div>
         </Scrollytelling.Pin>
       </section>
     </Scrollytelling.Root>
